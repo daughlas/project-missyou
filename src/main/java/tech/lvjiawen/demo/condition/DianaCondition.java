84158fb31c02344eb46343af020b2135135a46ef
daughlas <daughlas@gmail.com>
2020-08-21 16:47:42 +0800

    use @Conditional with conditionContext

--- src/main/java/tech/lvjiawen/demo/condition/DianaCondition.java
+++ src/main/java/tech/lvjiawen/demo/condition/DianaCondition.java
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
 public class DianaCondition implements Condition {
-
     public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
-        return true;
+        String name = conditionContext.getEnvironment().getProperty("hero.condition");
+        return "diana".equalsIgnoreCase(name);
     }
 }
